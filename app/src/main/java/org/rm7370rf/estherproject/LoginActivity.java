9a1180963aba7a903b12a41d0dcda327b2697d35
RM7370RF <rm7370rf@yandex.ru>
2019-05-30 20:14:09 +0300

    Added AlertDialog for creating/importing account

--- app/src/main/java/org/rm7370rf/estherproject/LoginActivity.java
+++ app/src/main/java/org/rm7370rf/estherproject/LoginActivity.java
@@ -3,8 +3,10 @@ package org.rm7370rf.estherproject;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
@@ -12,17 +14,31 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
     }
 
     @OnClick({R.id.createAccountBtn, R.id.importAccountBtn})
-    private void onClick(View view) {
+    public void onClick(View view) {
+        int dialogId;
+
         switch (view.getId()) {
             case R.id.createAccountBtn:
-
+                dialogId = R.layout.dialog_create_account;
                 break;
             case R.id.importAccountBtn:
-
+                dialogId = R.layout.dialog_import_account;
                 break;
+            default:
+                return;
         }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View builderView = getLayoutInflater().inflate(dialogId, null);
+        builderView.setOnClickListener(v -> {
+
+        });
+
+        builder.setView(builderView);
+        builder.create().show();
     }
 }
