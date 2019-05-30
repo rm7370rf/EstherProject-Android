8bf4ef7f86686b3823bf8d3a25413685ea082e18
RM7370RF <rm7370rf@yandex.ru>
2019-05-30 14:22:09 +0300

    Prepared UI of LoginActivity

--- app/src/main/java/org/rm7370rf/estherproject/LoginActivity.java
+++ app/src/main/java/org/rm7370rf/estherproject/LoginActivity.java
@@ -1,13 +1,28 @@
 package org.rm7370rf.estherproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @OnClick({R.id.createAccountBtn, R.id.importAccountBtn})
+    private void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.createAccountBtn:
+
+                break;
+            case R.id.importAccountBtn:
+
+                break;
+        }
     }
 }
