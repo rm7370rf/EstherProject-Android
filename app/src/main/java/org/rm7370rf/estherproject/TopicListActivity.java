99ca471205d613c75455d47bdb31cc7bfba20e47
RM7370RF <rm7370rf@yandex.ru>
2019-06-01 15:19:54 +0300

    Added menu to activity

--- app/src/main/java/org/rm7370rf/estherproject/TopicListActivity.java
+++ app/src/main/java/org/rm7370rf/estherproject/TopicListActivity.java
@@ -3,6 +3,10 @@ package org.rm7370rf.estherproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import static org.rm7370rf.estherproject.R.string.topics;
 
@@ -12,6 +16,35 @@ public class TopicListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topic_list);
         setTitle(topics);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_topic_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.accountData:
+                Log.d("MENU", "ACCOUNT_DATA");
+                return true;
+            case R.id.setUsername:
+                Log.d("MENU", "SET_USERNAME");
+                return true;
+            case R.id.logout:
+                Log.d("MENU", "LOGOUT");
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
