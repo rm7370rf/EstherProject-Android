29f71d41ec55407d1479e282f2b6f4d8c3958220
RM7370RF <rm7370rf@yandex.ru>
2019-06-01 17:09:39 +0300

    Added class for call/execute to contract

---
+++ app/src/main/java/org/rm7370rf/estherproject/contract/ContractManager.java
@@ -0,0 +1,72 @@
+package org.rm7370rf.estherproject.contract;
+
+import org.rm7370rf.estherproject.model.Account;
+import org.web3j.abi.FunctionEncoder;
+import org.web3j.abi.datatypes.Function;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.RawTransaction;
+import org.web3j.crypto.TransactionEncoder;
+import org.web3j.crypto.WalletUtils;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.request.Transaction;
+import org.web3j.protocol.core.methods.response.EthCall;
+import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
+import org.web3j.protocol.core.methods.response.EthSendTransaction;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.utils.Numeric;
+
+import java.math.BigInteger;
+
+import static org.rm7370rf.estherproject.utils.Config.CONTRACT_ADDRESS;
+import static org.rm7370rf.estherproject.utils.Config.NODE;
+import static org.web3j.tx.gas.DefaultGasProvider.GAS_LIMIT;
+import static org.web3j.tx.gas.DefaultGasProvider.GAS_PRICE;
+
+public class ContractManager {
+    private final Web3j web3j;
+    private final Account account;
+
+    public ContractManager(Account account) {
+        this.web3j = Web3j.build(new HttpService(NODE));
+        this.account = account;
+    }
+
+    private String callFunction(Function function) throws Exception {
+        String encodedFunction = FunctionEncoder.encode(function);
+
+        EthCall response = web3j.ethCall(
+                Transaction.createEthCallTransaction(account.getWalletAddress(), CONTRACT_ADDRESS, encodedFunction),
+                DefaultBlockParameterName.LATEST)
+                .sendAsync().get();
+
+        return response.getValue();
+    }
+
+    private String executeFunction(String password, Function function, String contractAddress) throws Exception {
+        Credentials credentials = WalletUtils.loadCredentials(password, account.getWalletFolder() + account.getWalletName());
+        BigInteger nonce = getNonce(credentials.getAddress());
+
+        String encodedFunction = FunctionEncoder.encode(function);
+
+        RawTransaction rawTransaction = RawTransaction.createTransaction(
+                nonce,
+                GAS_PRICE,
+                GAS_LIMIT,
+                contractAddress,
+                encodedFunction);
+
+        byte[] signedMessage = TransactionEncoder.signMessage(rawTransaction, credentials);
+        String hexValue = Numeric.toHexString(signedMessage);
+
+        EthSendTransaction transactionResponse = web3j.ethSendRawTransaction(hexValue)
+                .sendAsync().get();
+
+        return transactionResponse.getTransactionHash();
+    }
+
+    private BigInteger getNonce(String address) throws Exception {
+        EthGetTransactionCount ethGetTransactionCount = web3j.ethGetTransactionCount(address, DefaultBlockParameterName.LATEST).sendAsync().get();
+        return ethGetTransactionCount.getTransactionCount();
+    }
+}
