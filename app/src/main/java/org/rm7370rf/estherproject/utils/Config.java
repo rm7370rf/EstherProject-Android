8b5aba8c3bdf9dc2c0829037aa99e412de63b3c3
RM7370RF <rm7370rf@yandex.ru>
2019-06-01 17:08:54 +0300

    Minor changes

--- app/src/main/java/org/rm7370rf/estherproject/utils/Config.java
+++ app/src/main/java/org/rm7370rf/estherproject/utils/Config.java
@@ -1,6 +1,6 @@
 package org.rm7370rf.estherproject.utils;
 
 public class Config {
-    public static final String WALLET = "wallet";
-    public static final String PREFS = "prefs";
+    public static final String CONTRACT_ADDRESS = "0x";
+    public static final String NODE = "http://";
 }
