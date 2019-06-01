8ade53dd879a218575335b3d0c5165b86cafe4aa
RM7370RF <rm7370rf@yandex.ru>
2019-06-01 17:53:20 +0300

    Fix typo

--- app/src/main/java/org/rm7370rf/estherproject/utils/Config.java
+++ app/src/main/java/org/rm7370rf/estherproject/utils/Config.java
@@ -2,5 +2,5 @@ package org.rm7370rf.estherproject.utils;
 
 public class Config {
     public static final String CONTRACT_ADDRESS = "0xCeE85ca8E1FbE1eB1c4f1378A0Ca3F7E881Faa56";
-    public static final String NODE = "https://rinkeby.infura.io:8545";
+    public static final String NODE = "https://rinkeby.infura.io";
 }
