cf20e3011cb86aaa351d35134f46195e1b5016a2
RM7370RF <rm7370rf@yandex.ru>
2019-06-03 09:24:20 +0300

    Added dialog with account data & added CompositeDisposable to store disposables

--- app/src/main/java/org/rm7370rf/estherproject/ui/TopicListActivity.java
+++ app/src/main/java/org/rm7370rf/estherproject/ui/TopicListActivity.java
@@ -1,5 +1,6 @@
 package org.rm7370rf.estherproject.ui;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -8,8 +9,9 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ListView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import org.rm7370rf.estherproject.R;
 import org.rm7370rf.estherproject.contract.Contract;
@@ -17,17 +19,19 @@ import org.rm7370rf.estherproject.contract.model.Topic;
 import org.rm7370rf.estherproject.model.Account;
 import org.rm7370rf.estherproject.utils.Toast;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
+import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableSingleObserver;
 import io.reactivex.schedulers.Schedulers;
 import io.realm.Realm;
-import io.realm.RealmResults;
 
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -46,10 +50,11 @@ public class TopicListActivity extends AppCompatActivity {
     @BindView(R.id.recyclerView)
     RecyclerView recyclerView;
 
-    private Disposable disposable;
+    private CompositeDisposable disposables = new CompositeDisposable();
     private Contract contract;
     private TRVAdapter adapter;
     private Realm realm = Realm.getDefaultInstance();
+    private Account account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,7 +69,7 @@ public class TopicListActivity extends AppCompatActivity {
     }
 
     private void setContract() {
-        Account account = realm.where(Account.class).findFirst();
+        this.account = realm.where(Account.class).findFirst();
         this.contract = new Contract(realm.copyFromRealm(account));
         Log.d("ADDRESS", account.getWalletAddress());
         Log.d("FOLDER", account.getWalletFolder());
@@ -85,62 +90,64 @@ public class TopicListActivity extends AppCompatActivity {
     }
 
     private void updateDB(boolean bySwipe) {
-        this.disposable = Observable.create((ObservableEmitter<Topic> emitter) -> {
-            try {
-                BigInteger numberOfTopics = contract.countTopics();
-                long count = Realm.getDefaultInstance().where(Topic.class).count();
-                Log.d("RF", "|" + count + "|" + numberOfTopics + "|");
-                BigInteger localNumberOfTopics = BigInteger.valueOf(count);
+        disposables.add(
+                Observable.create((ObservableEmitter<Topic> emitter) -> {
+                    try {
+                        BigInteger numberOfTopics = contract.countTopics();
+                        long count = Realm.getDefaultInstance().where(Topic.class).count();
+                        Log.d("RF", "|" + count + "|" + numberOfTopics + "|");
+                        BigInteger localNumberOfTopics = BigInteger.valueOf(count);
 
-                if (numberOfTopics.compareTo(localNumberOfTopics) > 0) {
-                    for (BigInteger i = localNumberOfTopics; i.compareTo(numberOfTopics) < 0; i = i.add(BigInteger.ONE)) {
-                        Topic topic = contract.getTopic(i);
-                        emitter.onNext(topic);
-                    }
-                }
-                emitter.onComplete();
-            } catch (Exception e) {
-                e.printStackTrace();
-                emitter.onError(e);
-            }
-        }).subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        topic -> {
-                            Log.d("RF", "TopicId: " + (topic.getId()));
-                            Realm.getDefaultInstance().executeTransaction(realm -> realm.copyToRealm(topic));
-                        },
-                        error -> {
-                            Log.d("RF", "EXCEPTION");
-                            error.printStackTrace();
-                            Toast.show(this, error.getLocalizedMessage());
-                        },
-                        () -> {
-                            Log.d("RF", "GONE");
-                            if(!bySwipe) {
-                                progressBar.setVisibility(View.GONE);
-                                recyclerView.setVisibility(View.VISIBLE);
-                            }
-                            else {
-                                swipeRefreshLayout.setRefreshing(false);
-                            }
-
-                        },
-                        i -> {
-                            Log.d("RF", "VISIBLE");
-                            if(!bySwipe) {
-                                progressBar.setVisibility(View.VISIBLE);
-                                recyclerView.setVisibility(View.GONE);
+                        if (numberOfTopics.compareTo(localNumberOfTopics) > 0) {
+                            for (BigInteger i = localNumberOfTopics; i.compareTo(numberOfTopics) < 0; i = i.add(BigInteger.ONE)) {
+                                Topic topic = contract.getTopic(i);
+                                emitter.onNext(topic);
                             }
                         }
-                );
+                        emitter.onComplete();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        emitter.onError(e);
+                    }
+                }).subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(
+                                topic -> {
+                                    Log.d("RF", "TopicId: " + (topic.getId()));
+                                    Realm.getDefaultInstance().executeTransaction(realm -> realm.copyToRealm(topic));
+                                },
+                                error -> {
+                                    Log.d("RF", "EXCEPTION");
+                                    error.printStackTrace();
+                                    Toast.show(this, error.getLocalizedMessage());
+                                },
+                                () -> {
+                                    Log.d("RF", "GONE");
+                                    if(!bySwipe) {
+                                        progressBar.setVisibility(View.GONE);
+                                        recyclerView.setVisibility(View.VISIBLE);
+                                    }
+                                    else {
+                                        swipeRefreshLayout.setRefreshing(false);
+                                    }
+
+                                },
+                                i -> {
+                                    Log.d("RF", "VISIBLE");
+                                    if(!bySwipe) {
+                                        progressBar.setVisibility(View.VISIBLE);
+                                        recyclerView.setVisibility(View.GONE);
+                                    }
+                                }
+                        )
+        );
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if(!disposable.isDisposed()) {
-            disposable.dispose();
+        if(!disposables.isDisposed()) {
+            disposables.dispose();
         }
     }
 
@@ -149,7 +156,7 @@ public class TopicListActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_topic_list, menu);
 
-        boolean hasUsername = realm.where(Account.class).findFirst().hasUsername();
+        boolean hasUsername = account.hasUsername();
 
         MenuItem setUsernameItem = menu.findItem(R.id.setUsername);
         setUsernameItem.setVisible(!hasUsername);
@@ -166,9 +173,11 @@ public class TopicListActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.accountData:
                 Log.d("MENU", "ACCOUNT_DATA");
+                showAccountDataDialog();
                 return true;
             case R.id.setUsername:
                 Log.d("MENU", "SET_USERNAME");
+                showSetUsernameDialog();
                 return true;
             case R.id.logout:
                 Log.d("MENU", "LOGOUT");
@@ -176,5 +185,80 @@ public class TopicListActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void showAccountDataDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.dialog_account_data, null);
+
+        TextView userBalanceText = view.findViewById(R.id.userBalance);
+        TextView userNameLabel = view.findViewById(R.id.userNameLabel);
+        TextView userNameText = view.findViewById(R.id.userName);
+        TextView userAddressText = view.findViewById(R.id.userAddress);
+
+        userBalanceText.setText(String.valueOf(account.getBalance()));
+        int userNameVisibility = account.hasUsername() ? View.VISIBLE : View.GONE;
+        userNameLabel.setVisibility(userNameVisibility);
+        userNameText.setVisibility(userNameVisibility);
+
+        if(!account.hasUsername()) {
+            userNameText.setText(account.getUserName());
+        }
+
+        userAddressText.setText(account.getWalletAddress());
+
+        SwipeRefreshLayout swipeRefreshLayout = view.findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(() -> refreshUserData(view, userBalanceText, swipeRefreshLayout, true));
+
+        refreshUserData(view, userBalanceText, swipeRefreshLayout, false);
+
+        builder.setView(view);
+        AlertDialog dialog = builder.create();
+        dialog.setCancelable(true);
+        dialog.show();
+    }
+
+    private void refreshUserData(View view, TextView userBalanceText, SwipeRefreshLayout swipeRefreshLayout, boolean bySwipe) {
+        ProgressBar progressBar = view.findViewById(R.id.progressBar);
+
+        Single<BigDecimal> single = Single.fromCallable(() -> contract.getBalance())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        disposables.add(
+                single.subscribeWith(new DisposableSingleObserver<BigDecimal>() {
+                    @Override
+                    protected void onStart() {
+                        super.onStart();
+                        if(!bySwipe) {
+                            progressBar.setVisibility(View.VISIBLE);
+                        }
+                    }
+                    @Override
+                    public void onSuccess(BigDecimal balance) {
+                        userBalanceText.setText(String.valueOf(balance));
+                        onComplete();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Toast.show(view.getContext(), e.getLocalizedMessage());
+                        onComplete();
+                    }
+
+                    private void onComplete() {
+                        if(!bySwipe) {
+                            progressBar.setVisibility(View.GONE);
+                        }
+                        else {
+                            swipeRefreshLayout.setRefreshing(false);
+                        }
+                    }
+                })
+        );
+    }
+
+    private void showSetUsernameDialog() {
+
     }
 }
