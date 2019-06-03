7d7f559d14bd8b6af8cf21cd1e80cbfac395f30c
RM7370RF <rm7370rf@yandex.ru>
2019-06-03 18:17:46 +0300

    Renamed objects

--- app/src/main/java/org/rm7370rf/estherproject/ui/TopicsAdapter.java
+++ app/src/main/java/org/rm7370rf/estherproject/ui/TopicsAdapter.java
@@ -12,30 +12,38 @@ import org.apache.commons.lang3.StringUtils;
 import org.rm7370rf.estherproject.R;
 import org.rm7370rf.estherproject.model.Topic;
 
+import java.math.BigInteger;
+
 import io.realm.OrderedRealmCollection;
 import io.realm.RealmRecyclerViewAdapter;
 
 import static org.rm7370rf.estherproject.utils.Config.MAX_LIST_ITEM_TEXT_LENGTH;
 
 public class TopicsAdapter extends RealmRecyclerViewAdapter<Topic, TopicsAdapter.ViewHolder> {
+    private OnItemClickListener listener;
+    private int selectedPos = RecyclerView.NO_POSITION;
+
     public TopicsAdapter(OrderedRealmCollection<Topic> data) {
         super(data, true);
         setHasStableIds(true);
+    }
+
+    public void setListener(OnItemClickListener listener) {
+        this.listener = listener;
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.topics_recyclerview_item, parent, false);
+
         return new ViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        final Topic topic = getItem(position);
-        holder.itemNumberText.setText(String.valueOf(position + 1));
-        holder.itemSubjectText.setText(StringUtils.abbreviate(topic.getSubject(), MAX_LIST_ITEM_TEXT_LENGTH));
-        holder.itemMessageText.setText(StringUtils.abbreviate(topic.getMessage(), MAX_LIST_ITEM_TEXT_LENGTH));
+        holder.bind(position);
+        holder.itemView.setSelected(selectedPos == position);
     }
 
     @Override
@@ -45,15 +53,31 @@ public class TopicsAdapter extends RealmRecyclerViewAdapter<Topic, TopicsAdapter
 
 
     class ViewHolder extends RecyclerView.ViewHolder {
-        TextView itemNumberText;
-        TextView itemSubjectText;
-        TextView itemMessageText;
+        TextView numberText;
+        TextView subjectText;
+        TextView messageText;
 
         ViewHolder(View view) {
             super(view);
-            itemNumberText = view.findViewById(R.id.itemNumber);
-            itemSubjectText = view.findViewById(R.id.itemSubject);
-            itemMessageText = view.findViewById(R.id.itemMessage);
+            numberText = view.findViewById(R.id.numberText);
+            subjectText = view.findViewById(R.id.subjectText);
+            messageText = view.findViewById(R.id.messageText);
         }
+
+        public void bind(int position) {
+            final Topic topic = getItem(position);
+            numberText.setText(String.valueOf(position + 1));
+            subjectText.setText(StringUtils.abbreviate(topic.getSubject(), MAX_LIST_ITEM_TEXT_LENGTH));
+            messageText.setText(StringUtils.abbreviate(topic.getMessage(), MAX_LIST_ITEM_TEXT_LENGTH));
+            itemView.setOnClickListener(v -> {
+                selectedPos = getAdapterPosition();
+                notifyItemChanged(getAdapterPosition());
+                listener.onClick(topic.getId());
+            });
+        }
+    }
+
+    interface OnItemClickListener {
+        void onClick(BigInteger topicId);
     }
 }
