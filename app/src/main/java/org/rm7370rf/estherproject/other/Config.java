4c8f4ba0d9eec32d5b6f040c5fc10d8748d0e138
RM7370RF <rm7370rf@yandex.ru>
2019-06-05 15:17:01 +0300

    Changed contract address

--- app/src/main/java/org/rm7370rf/estherproject/other/Config.java
+++ app/src/main/java/org/rm7370rf/estherproject/other/Config.java
@@ -1,7 +1,7 @@
 package org.rm7370rf.estherproject.other;
 
 public class Config {
-    public static final String CONTRACT_ADDRESS = "0xCeE85ca8E1FbE1eB1c4f1378A0Ca3F7E881Faa56";
+    public static final String CONTRACT_ADDRESS = "0x6ac6f0ab210565a0e12613912267b071054fc51f";
     public static final String NODE = "https://rinkeby.infura.io";
     public static final int MAX_LIST_ITEM_TEXT_LENGTH = 30;
 }
