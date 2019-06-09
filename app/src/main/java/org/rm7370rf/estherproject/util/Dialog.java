975036bc6ee1afe50cc644b064c7557c3246b716
RM7370RF <rm7370rf@yandex.ru>
2019-06-09 17:18:55 +0300

    Added onDismiss method

--- app/src/main/java/org/rm7370rf/estherproject/util/Dialog.java
+++ app/src/main/java/org/rm7370rf/estherproject/util/Dialog.java
@@ -2,18 +2,20 @@ package org.rm7370rf.estherproject.util;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 
-public class Dialog extends AlertDialog.Builder {
+public class Dialog extends AlertDialog.Builder implements DialogInterface.OnDismissListener {
     private Activity activity;
     private AlertDialog dialog;
     private View view;
 
     public Dialog(@NonNull Activity activity) {
         super(activity);
+        setOnDismissListener(this);
         this.activity = activity;
     }
 
@@ -44,4 +46,7 @@ public class Dialog extends AlertDialog.Builder {
         dialog.show();
         return dialog;
     }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) { }
 }
