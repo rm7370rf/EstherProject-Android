e0bc266b332e21e77589570f978ada1aca23f1fc
RM7370RF <rm7370rf@yandex.ru>
2019-06-12 09:45:21 +0300

    Set StrategyType

--- app/src/main/java/org/rm7370rf/estherproject/ui/view/SetUsernameView.java
+++ app/src/main/java/org/rm7370rf/estherproject/ui/view/SetUsernameView.java
@@ -1,5 +1,9 @@
 package org.rm7370rf.estherproject.ui.view;
 
+import moxy.viewstate.strategy.AddToEndSingleStrategy;
+import moxy.viewstate.strategy.StateStrategyType;
+
+@StateStrategyType(value = AddToEndSingleStrategy.class)
 public interface SetUsernameView extends DialogView {
     void onComplete();
 }
