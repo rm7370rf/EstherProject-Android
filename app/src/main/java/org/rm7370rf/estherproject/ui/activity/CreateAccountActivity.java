671eb0ee5483222adf9f7972aa47c61bca4d3b4d
RM7370RF <rm7370rf@yandex.ru>
2019-06-12 15:48:41 +0300

    Added using of createIntentWithoutHistory function

--- app/src/main/java/org/rm7370rf/estherproject/ui/activity/CreateAccountActivity.java
+++ app/src/main/java/org/rm7370rf/estherproject/ui/activity/CreateAccountActivity.java
@@ -10,6 +10,7 @@ import org.rm7370rf.estherproject.ui.dialog.CreateAccountDialog;
 import butterknife.ButterKnife;
 import moxy.MvpAppCompatActivity;
 
+import static org.rm7370rf.estherproject.util.Utils.createIntentWithoutHistory;
 import static org.rm7370rf.estherproject.util.Verifier.isAccountExists;
 
 public class CreateAccountActivity extends MvpAppCompatActivity {
@@ -25,10 +26,7 @@ public class CreateAccountActivity extends MvpAppCompatActivity {
     }
 
     private void startTopicListActivity() {
-        Intent intent = new Intent(this, TopicListActivity.class)
-               .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-
-        startActivity(intent);
+        startActivity(createIntentWithoutHistory(this, TopicListActivity.class));
     }
 
     public void onClick(View view) {
