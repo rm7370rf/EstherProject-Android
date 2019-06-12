d4c240f4234870d2af15a4125d540d46ed626ab2
RM7370RF <rm7370rf@yandex.ru>
2019-06-12 09:44:28 +0300

    AddPostDialog now based on MVP pattern

---
+++ app/src/main/java/org/rm7370rf/estherproject/ui/dialog/AddPostDialog.java
@@ -0,0 +1,59 @@
+package org.rm7370rf.estherproject.ui.dialog;
+
+import org.rm7370rf.estherproject.R;
+import org.rm7370rf.estherproject.ui.presenter.AddPostPresenter;
+import org.rm7370rf.estherproject.ui.view.DialogView;
+import org.rm7370rf.estherproject.util.FieldDialog;
+import org.rm7370rf.estherproject.util.Toast;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import moxy.presenter.InjectPresenter;
+
+import static org.rm7370rf.estherproject.R.string.send;
+
+public class AddPostDialog extends FieldDialog implements DialogView, CreateAccountDialog.OnClickListener {
+    @InjectPresenter
+    AddPostPresenter presenter;
+    private BigInteger topicId;
+
+    public AddPostDialog() {
+        setLayout(R.layout.dialog_add_post);
+        setOnClickListener(send, this);
+    }
+
+    public void setTopicId(BigInteger topicId) {
+        this.topicId = topicId;
+    }
+
+    @Override
+    public void showToast(int resource) {
+        showToast(getString(resource));
+    }
+
+    @Override
+    public void showToast(String message) {
+        Toast.show(getContext(), message);
+    }
+
+    @Override
+    public void collapsePositiveButton() {
+        super.collapsePositiveButton();
+    }
+
+    @Override
+    public void expandPositiveButton() {
+        super.expandPositiveButton();
+    }
+
+    @Override
+    protected void fillEditText() {
+        addToEditTextList(R.id.messageEdit, R.id.passwordEdit);
+    }
+
+    @Override
+    public void onClick(int buttonId, List<String> valueList) {
+        presenter.onClick(topicId, valueList);
+    }
+}
---
+++ app/src/main/java/org/rm7370rf/estherproject/ui/presenter/AddPostPresenter.java
@@ -0,0 +1,62 @@
+package org.rm7370rf.estherproject.ui.presenter;
+
+import org.rm7370rf.estherproject.contract.Contract;
+import org.rm7370rf.estherproject.ui.view.DialogView;
+import org.rm7370rf.estherproject.util.FieldDialog;
+import org.rm7370rf.estherproject.util.Toast;
+import org.rm7370rf.estherproject.util.Verifier;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import io.reactivex.Completable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableCompletableObserver;
+import io.reactivex.schedulers.Schedulers;
+import moxy.MvpPresenter;
+
+import static org.rm7370rf.estherproject.R.string.request_successfully_sent;
+
+public class AddPostPresenter extends MvpPresenter<DialogView> {
+    private Disposable disposable;
+    private Contract contract = Contract.getInstance();
+
+    public void onClick(BigInteger topicId, List<String> valueList) {
+        String message = valueList.get(0);
+        String password = valueList.get(1);
+
+        disposable = Completable.fromAction(() -> {
+            Verifier.verifyMessage(message);
+            Verifier.verifyPassword(password);
+            contract.addPostToTopic(password, topicId, message);
+        })
+        .subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribeWith(new DisposableCompletableObserver() {
+            @Override
+            protected void onStart() {
+                getViewState().collapsePositiveButton();
+            }
+
+            @Override
+            public void onComplete() {
+                getViewState().showToast(request_successfully_sent);
+                getViewState().expandPositiveButton();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                e.printStackTrace();
+                getViewState().showToast(e.getLocalizedMessage());
+                getViewState().expandPositiveButton();
+            }
+        });
+    }
+
+    @Override
+    public void onDestroy() {
+        disposable.dispose();
+        super.onDestroy();
+    }
+}
