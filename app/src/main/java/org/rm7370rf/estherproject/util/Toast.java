505ee39096aca2e4a2b95b91a154efdf08887cf7
RM7370RF <rm7370rf@yandex.ru>
2019-06-12 20:37:03 +0300

    Added Toast function with Throwable as message

--- app/src/main/java/org/rm7370rf/estherproject/util/Toast.java
+++ app/src/main/java/org/rm7370rf/estherproject/util/Toast.java
@@ -2,10 +2,18 @@ package org.rm7370rf.estherproject.util;
 
 import android.content.Context;
 
+import org.rm7370rf.estherproject.expception.VerifierException;
+
 public class Toast {
     public static void show(Context context, String text) {
         android.widget.Toast.makeText(context, text, android.widget.Toast.LENGTH_LONG).show();
     }
+
+    public static void show(Context context, Throwable e) {
+        String text = (e instanceof VerifierException) ? context.getString(((VerifierException) e).getResource()) : e.getLocalizedMessage();
+        android.widget.Toast.makeText(context, text, android.widget.Toast.LENGTH_LONG).show();
+    }
+
 
     public static void show(Context context, int resId) {
         show(context, context.getString(resId));
