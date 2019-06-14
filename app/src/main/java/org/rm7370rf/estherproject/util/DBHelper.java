0d4de415353297d571e470b564be5a43cf2b0743
RM7370RF <rm7370rf@yandex.ru>
2019-06-14 18:29:48 +0300

    Removed executeTransaction functions

--- app/src/main/java/org/rm7370rf/estherproject/util/DBHelper.java
+++ app/src/main/java/org/rm7370rf/estherproject/util/DBHelper.java
@@ -1,5 +1,6 @@
 package org.rm7370rf.estherproject.util;
 
+import org.apache.commons.lang3.concurrent.Computable;
 import org.rm7370rf.estherproject.EstherProject;
 import org.rm7370rf.estherproject.model.Post;
 import org.rm7370rf.estherproject.model.Topic;
@@ -9,45 +10,47 @@ import java.math.BigInteger;
 
 import javax.inject.Inject;
 
+import io.reactivex.Completable;
+import io.reactivex.CompletableObserver;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
+import io.reactivex.schedulers.Schedulers;
 import io.realm.Realm;
 import io.realm.RealmResults;
 import io.realm.Sort;
 
 public class DBHelper {
-    @Inject
-    Realm realm;
-
     public DBHelper() {
         EstherProject.getComponent().inject(this);
     }
 
-
-    public void executeTransaction(Realm.Transaction transaction) {
-        realm.executeTransactionAsync(transaction);
-    }
-
-    public void executeTransaction(Realm.Transaction transaction, Realm.Transaction.OnSuccess listener) {
-        realm.executeTransactionAsync(transaction, listener);
-    }
-
     public long countTopics() {
-        return realm.where(Topic.class).count();
+        try (Realm realm = Realm.getDefaultInstance()) {
+            return realm.where(Topic.class).count();
+        }
     }
 
     public long countPosts(BigInteger topicId) {
-        return realm.where(Post.class)
-                .equalTo(Keys.Db.TOPIC_ID, String.valueOf(topicId))
-                .count();
+        try (Realm realm = Realm.getDefaultInstance()) {
+            return realm.where(Post.class)
+                    .equalTo(Keys.Db.TOPIC_ID, String.valueOf(topicId))
+                    .count();
+        }
     }
 
     public Topic getTopic(BigInteger topicId) {
-        return realm.where(Topic.class).equalTo(Keys.Db.TOPIC_ID, String.valueOf(topicId)).findFirst();
+        try (Realm realm = Realm.getDefaultInstance()) {
+            return realm.where(Topic.class).equalTo(Keys.Db.TOPIC_ID, String.valueOf(topicId)).findFirst();
+        }
     }
 
     public RealmResults<Post> getPosts(BigInteger topicId) {
-        return realm.where(Post.class)
-                .equalTo(Keys.Db.TOPIC_ID, String.valueOf(topicId))
-                .sort(Keys.Db.TIMESTAMP, Sort.ASCENDING)
-                .findAll();
+        try (Realm realm = Realm.getDefaultInstance()) {
+            return realm.where(Post.class)
+                    .equalTo(Keys.Db.TOPIC_ID, String.valueOf(topicId))
+                    .sort(Keys.Db.TIMESTAMP, Sort.ASCENDING)
+                    .findAll();
+        }
     }
 }
