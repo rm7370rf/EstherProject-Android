e57541186d45bac25ac626cfe384a1586c9c7aca
RM7370RF <rm7370rf@yandex.ru>
2019-06-14 12:57:49 +0300

    Added RealmModule

--- app/src/main/java/org/rm7370rf/estherproject/EstherProject.java
+++ app/src/main/java/org/rm7370rf/estherproject/EstherProject.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import org.rm7370rf.estherproject.di.AppComponent;
 import org.rm7370rf.estherproject.di.ContractModule;
 import org.rm7370rf.estherproject.di.DaggerAppComponent;
+import org.rm7370rf.estherproject.di.RealmModule;
 import org.rm7370rf.estherproject.di.RefreshAnimationUtilModule;
 import org.rm7370rf.estherproject.model.Account;
 import org.rm7370rf.estherproject.service.BalanceService;
@@ -18,7 +19,6 @@ public class EstherProject extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Realm.init(this);
         component = buildComponent();
         if(Account.get() != null) {
             startService(new Intent(this, BalanceService.class));
@@ -32,6 +32,7 @@ public class EstherProject extends Application {
     protected AppComponent buildComponent() {
         return DaggerAppComponent
                 .builder()
+                .realmModule(new RealmModule(this))
                 .contractModule(new ContractModule())
                 .refreshAnimationUtilModule(new RefreshAnimationUtilModule())
                 .build();
--- app/src/main/java/org/rm7370rf/estherproject/di/AppComponent.java
+++ app/src/main/java/org/rm7370rf/estherproject/di/AppComponent.java
@@ -15,7 +15,7 @@ import javax.inject.Singleton;
 
 import dagger.Component;
 
-@Component(modules = {ContractModule.class, RefreshAnimationUtilModule.class})
+@Component(modules = {ContractModule.class, RefreshAnimationUtilModule.class, RealmModule.class})
 @Singleton
 public interface AppComponent {
     void inject(TopicActivity activity);
