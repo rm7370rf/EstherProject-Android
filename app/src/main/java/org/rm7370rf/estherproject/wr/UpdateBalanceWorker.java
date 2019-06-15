450316993518b986d842a62f87bc7e93083e875d
RM7370RF <rm7370rf@yandex.ru>
2019-06-15 09:30:52 +0300

    Added inject support in UpdateBalanceWorker

--- app/src/main/java/org/rm7370rf/estherproject/wr/UpdateBalanceWorker.java
+++ app/src/main/java/org/rm7370rf/estherproject/wr/UpdateBalanceWorker.java
@@ -3,12 +3,14 @@ package org.rm7370rf.estherproject.wr;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import org.rm7370rf.estherproject.EstherProject;
 import org.rm7370rf.estherproject.R;
 import org.rm7370rf.estherproject.model.Account;
 import org.rm7370rf.estherproject.other.Config;
@@ -25,12 +27,14 @@ public class UpdateBalanceWorker extends Worker {
 
     public UpdateBalanceWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
+        EstherProject.getComponent().inject(this);
     }
 
     @NonNull
     @Override
     public Result doWork() {
         try {
+            Log.d(Config.UPD_BALANCE_TAG, "@doWork");
             if(receiverUtil.loadNewBalanceToDatabase()) {
                 Notification.show(getApplicationContext(), Config.BALANCE_NOTIFICATION_ID, R.string.app_name, R.string.changed_balance);
             }
