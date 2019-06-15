41f25f78618b1889f3241a5ed18815d5ccc1e658
RM7370RF <rm7370rf@yandex.ru>
2019-06-15 09:25:26 +0300

    Added TAG for updating balance

--- app/src/main/java/org/rm7370rf/estherproject/other/Config.java
+++ app/src/main/java/org/rm7370rf/estherproject/other/Config.java
@@ -2,6 +2,7 @@ package org.rm7370rf.estherproject.other;
 
 public class Config {
     public static final String UPD_TOPIC_TAG = "UPD_TOPIC_TAG";
+    public static final String UPD_BALANCE_TAG = "UPD_BALANCE_TAG";
     public static final String NOTIFICATION_CHANNEL = "NOTIFICATION_CHANNEL";
     public static final String CONTRACT_ADDRESS = "0x6ac6f0ab210565a0e12613912267b071054fc51f";
     public static final String NODE = "https://rinkeby.infura.io";
