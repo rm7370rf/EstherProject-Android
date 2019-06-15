7b96f2e3b3f5e97ed89a93d096325677f75194c3
RM7370RF <rm7370rf@yandex.ru>
2019-06-15 09:26:47 +0300

    Created function buildPeriodicWorkRequest & Added worker to queue for updating (checking) balance

--- app/src/main/java/org/rm7370rf/estherproject/wr/RefreshScheduler.java
+++ app/src/main/java/org/rm7370rf/estherproject/wr/RefreshScheduler.java
@@ -1,11 +1,13 @@
 package org.rm7370rf.estherproject.wr;
 
 import androidx.work.Constraints;
+import androidx.work.ListenableWorker;
 import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkManager;
 
 import org.rm7370rf.estherproject.other.Config;
 
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import static androidx.work.NetworkType.CONNECTED;
@@ -18,17 +20,21 @@ public class RefreshScheduler {
     }
 
     public static void prepareWorkManager() {
-        PeriodicWorkRequest request = buildUpdateTopicWork();
-        WorkManager.getInstance().enqueue(request);
+        PeriodicWorkRequest updateTopicRequest = buildPeriodicWorkRequest(Config.UPD_TOPIC_TAG, UpdateTopicsWorker.class);
+        PeriodicWorkRequest balanceTopicRequest = buildPeriodicWorkRequest(Config.UPD_BALANCE_TAG, UpdateBalanceWorker.class);
+        WorkManager.getInstance().enqueue(Arrays.asList(
+                updateTopicRequest,
+                balanceTopicRequest
+        ));
     }
 
     public static void cancelAll() {
         WorkManager.getInstance().cancelAllWork();
     }
 
-    private static PeriodicWorkRequest buildUpdateTopicWork() {
-        return new PeriodicWorkRequest.Builder(UpdateTopicsWorker.class, 15, TimeUnit.MINUTES)
-                .addTag(Config.UPD_TOPIC_TAG)
+    private static PeriodicWorkRequest buildPeriodicWorkRequest(String tag, Class<? extends ListenableWorker> worker) {
+        return new PeriodicWorkRequest.Builder(worker, 15, TimeUnit.MINUTES)
+                .addTag(tag)
                 .setConstraints(buildConstraints())
                 .build();
     }
