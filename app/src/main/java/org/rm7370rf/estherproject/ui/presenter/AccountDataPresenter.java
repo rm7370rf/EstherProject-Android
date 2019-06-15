4e652115c91b024ff3d5d0361f40ad7a3ccdb486
RM7370RF <rm7370rf@yandex.ru>
2019-06-15 09:16:15 +0300

    Load balance code moved to ReceiverUtil & added change listener to account

--- app/src/main/java/org/rm7370rf/estherproject/ui/presenter/AccountDataPresenter.java
+++ app/src/main/java/org/rm7370rf/estherproject/ui/presenter/AccountDataPresenter.java
@@ -5,14 +5,17 @@ import org.rm7370rf.estherproject.contract.Contract;
 import org.rm7370rf.estherproject.model.Account;
 import org.rm7370rf.estherproject.ui.view.AccountDataView;
 import org.rm7370rf.estherproject.util.DBHelper;
+import org.rm7370rf.estherproject.util.ReceiverUtil;
 
 import java.math.BigDecimal;
 
 import javax.inject.Inject;
 
+import io.reactivex.Completable;
 import io.reactivex.Single;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableCompletableObserver;
 import io.reactivex.observers.DisposableSingleObserver;
 import io.reactivex.schedulers.Schedulers;
 import io.realm.Realm;
@@ -28,6 +31,11 @@ public class AccountDataPresenter extends MvpPresenter<AccountDataView> {
     @Inject
     DBHelper dbHelper;
 
+    @Inject
+    ReceiverUtil receiverUtil;
+
+    private Account account = Account.get();
+
     public AccountDataPresenter() {
         EstherProject.getComponent().inject(this);
     }
@@ -35,29 +43,26 @@ public class AccountDataPresenter extends MvpPresenter<AccountDataView> {
     @Override
     protected void onFirstViewAttach() {
         super.onFirstViewAttach();
-        getViewState().prepareView(Account.get());
+        getViewState().prepareView(account);
+        setBalanceListener();
+    }
+
+    private void setBalanceListener() {
+        account.addChangeListener(realmModel -> {
+            String balance = String.valueOf(account.getBalance());
+            getViewState().setBalance(balance);
+        });
     }
 
     public void refreshUserData(boolean bySwipe) {
-        disposable = Single.fromCallable(() -> {
-                    BigDecimal balance = contract.getBalance();
-                    try (Realm realm = Realm.getDefaultInstance()) {
-                        realm.executeTransaction(r -> Account.get().setBalance(balance));
-                    }
-                    return balance;
-                })
+        disposable = Completable.fromAction(() -> receiverUtil.loadNewBalanceToDatabase())
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribeWith(new DisposableSingleObserver<BigDecimal>() {
+                .subscribeWith(new DisposableCompletableObserver() {
                     @Override
                     protected void onStart() {
                         super.onStart();
                         getViewState().enabledLoading(bySwipe);
-                    }
-
-                    @Override
-                    public void onSuccess(BigDecimal balance) {
-                        getViewState().setBalance(String.valueOf(balance));
                     }
 
                     @Override
@@ -66,7 +71,8 @@ public class AccountDataPresenter extends MvpPresenter<AccountDataView> {
                         onComplete();
                     }
 
-                    private void onComplete() {
+                    @Override
+                    public void onComplete() {
                         getViewState().disableLoading(bySwipe);
                     }
                 });
