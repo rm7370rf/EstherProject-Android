5ba9ce8ad406820883d37416bb81ecfc5e5eabec
RM7370RF <rm7370rf@yandex.ru>
2019-06-15 09:15:28 +0300

    Added data for notification showing

--- app/src/main/java/org/rm7370rf/estherproject/other/Config.java
+++ app/src/main/java/org/rm7370rf/estherproject/other/Config.java
@@ -2,7 +2,9 @@ package org.rm7370rf.estherproject.other;
 
 public class Config {
     public static final String UPD_TOPIC_TAG = "UPD_TOPIC_TAG";
+    public static final String NOTIFICATION_CHANNEL = "NOTIFICATION_CHANNEL";
     public static final String CONTRACT_ADDRESS = "0x6ac6f0ab210565a0e12613912267b071054fc51f";
     public static final String NODE = "https://rinkeby.infura.io";
+    public static final int BALANCE_NOTIFICATION_ID = 1;
     public static final int MAX_LIST_ITEM_TEXT_LENGTH = 30;
 }
