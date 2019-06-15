07a86d1aa2340ce0d6b25d4a97140b316004e145
RM7370RF <rm7370rf@yandex.ru>
2019-06-15 09:17:20 +0300

    Added worker for updating (and checking) balance

---
+++ app/src/main/java/org/rm7370rf/estherproject/wr/UpdateBalanceWorker.java
@@ -0,0 +1,44 @@
+package org.rm7370rf.estherproject.wr;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import org.rm7370rf.estherproject.R;
+import org.rm7370rf.estherproject.model.Account;
+import org.rm7370rf.estherproject.other.Config;
+import org.rm7370rf.estherproject.util.Notification;
+import org.rm7370rf.estherproject.util.ReceiverUtil;
+
+import java.math.BigDecimal;
+
+import javax.inject.Inject;
+
+public class UpdateBalanceWorker extends Worker {
+    @Inject
+    ReceiverUtil receiverUtil;
+
+    public UpdateBalanceWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        try {
+            if(receiverUtil.loadNewBalanceToDatabase()) {
+                Notification.show(getApplicationContext(), Config.BALANCE_NOTIFICATION_ID, R.string.app_name, R.string.changed_balance);
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return Result.failure();
+        }
+        return Result.success();
+    }
+}
